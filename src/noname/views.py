efa5408bc701d56b2fa2340ca0bc56d916af7a2e
ggrobelny <ggrobelny@outlook.fr>
2022-01-21 23:55:04 +0100

    views.py my_list ->test condition if user.is_authenticated in index.html

--- src/noname/views.py
+++ src/noname/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 def home_page(request):
     my_title = "Help Desk Blog: "
-    context = {"title": my_title}
+    context = {"title": my_title, "my_list": [1, 2, 3, 4, 5]}
     return render(request, "index.html", context)
 
 
