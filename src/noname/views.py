5da25b4639af67a854e82f8e9eb8a6937ea57eb0
ggrobelny <ggrobelny@outlook.fr>
2022-01-22 00:04:35 +0100

    django-admin startapp blog

--- src/noname/views.py
+++ src/noname/views.py
@@ -4,7 +4,9 @@ from django.shortcuts import render
 
 def home_page(request):
     my_title = "Help Desk Blog: "
-    context = {"title": my_title, "my_list": [1, 2, 3, 4, 5]}
+    context = {"title": "Log in, please:"}
+    if request.user.is_authenticated:
+        context = {"title": my_title, "my_list": [1, 2, 3, 4, 5]}
     return render(request, "index.html", context)
 
 
