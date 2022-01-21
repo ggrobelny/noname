7486a34b73c00d73b0cd1ed9b44fb36c2f0be3c3
ggrobelny <ggrobelny@outlook.fr>
2022-01-21 21:14:05 +0100

    folder templates with file index.html, return render in views.py def home_page

--- src/noname/views.py
+++ src/noname/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home_page(request):
-    return HttpResponse("<h1>Help Desk Blog</1>")
+    return render(request, "index.html")
 
 
 def about_page(request):
