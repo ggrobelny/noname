f93ee5ba1cc3609227354fa8bae02df36d736855
ggrobelny <ggrobelny@outlook.fr>
2022-01-22 16:53:30 +0100

    blog_post_detail 1: function to fetch posts from db in blog/views.py

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import BlogPost
+
+obj = BlogPost.objects.get(id=1)
+
+
+def blog_post_detail_page(request):
+
+    template_name = 'blog_post_detail.html'
+    context = {}
+    return render(request, template_name, context)
