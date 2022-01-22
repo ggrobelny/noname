fcbf980fd55ad4df7d153170e51e9a9930659ff6
ggrobelny <ggrobelny@outlook.fr>
2022-01-22 21:48:51 +0100

    nesting blog/templates/blog hateemele

--- src/blog/views.py
+++ src/blog/views.py
@@ -8,14 +8,14 @@ def blog_post_list_view(request):
     # list of the objects
     # could be search
     qs = BlogPost.objects.all()
-    template_name = 'blog_post_list.html'
+    template_name = 'blog/list.html'
     context = {'object_list': qs}
     return render(request, template_name, context)
 
 
 def blog_post_create_view(request):
     # create the objects
-    template_name = 'blog_post_create.html'
+    template_name = 'blog/create.html'
     context = {'form': None}
     return render(request, template_name, context)
 
@@ -23,20 +23,20 @@ def blog_post_create_view(request):
 def blog_post_detail_view(request, slug):
     # 1 object -> detail view
     obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = 'blog_post_detail.html'
+    template_name = 'blog/detail.html'
     context = {'object': obj}
     return render(request, template_name, context)
 
 
 def blog_post_update_view(request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = 'blog_post_update.html'
+    template_name = 'blog/update.html'
     context = {'object': obj, 'form': None}
     return render(request, template_name, context)
 
 
 def blog_post_delete_view(request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = 'blog_post_delete.html'
+    template_name = 'blog/delete.html'
     context = {'object': obj}
     return render(request, template_name, context)
