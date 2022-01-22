8c50d5f60a48c816cdc211cfd131d82e884e1da1
ggrobelny <ggrobelny@outlook.fr>
2022-01-22 17:43:16 +0100

    blog_post_detail 4: import Http404, get_object_or_404, slug in models.py

--- src/blog/models.py
+++ src/blog/models.py
@@ -3,4 +3,5 @@ from django.db import models
 
 class BlogPost(models.Model):
     title = models.TextField()
+    slug = models.SlugField()
     content = models.TextField(null=True, blank=True)
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 # Create your views here.
 from .models import BlogPost
 
 
 def blog_post_detail_page(request, post_id):
-    obj = BlogPost.objects.get(id=post_id)   # query
+
+    obj = get_object_or_404(BlogPost, id=post_id)
     template_name = 'blog_post_detail.html'
     context = {"object": obj}
     return render(request, template_name, context)
