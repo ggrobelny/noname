afc77e24ee15bb35f598eab9f52ffaa69b86ad7d
ggrobelny <ggrobelny@outlook.fr>
2022-02-01 22:02:54 +0100

    foreign key user

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,7 +1,12 @@
+from operator import mod
+from django.conf import settings
 from django.db import models
 
 
+User = settings.AUTH_USER_MODEL
+
 class BlogPost(models.Model):
+    user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True)
     content = models.TextField(null=True, blank=True)
