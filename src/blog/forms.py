728f988e8e7e44c3a2c010217341860f75130734
ggrobelny <ggrobelny@outlook.fr>
2022-02-02 22:48:28 +0100

    delete update confirm and redirect after delete

--- src/blog/forms.py
+++ src/blog/forms.py
@@ -14,8 +14,13 @@ class BlogPostModelForm(forms.ModelForm):
         fields = ['title', 'slug', 'content']
 
     def clean_title(self, *args, **kwargs):
+        # print(dir(self))
+        instance = self.instance
+        # print(instance)
         title = self.cleaned_data.get('title')
         qs = BlogPost.objects.filter(title__iexact=title)
+        if instance is not None:
+            qs = qs.exclude(pk=instance.pk)  # id=instance.id
         if qs.exists():
             raise forms.ValidationError("Ooo Panie, to nie poprawne z twojej strony!")
         return title
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import Http404
 # Create your views here.
 from .models import BlogPost
@@ -39,16 +39,22 @@ def blog_post_detail_view(request, slug):
     context = {'object': obj}
     return render(request, template_name, context)
 
-
+@staff_member_required
 def blog_post_update_view(request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = 'blog/update.html'
-    context = {'object': obj, 'form': None}
+    form = BlogPostModelForm(request.POST or None, instance=obj)
+    if form.is_valid():
+        form.save()
+    template_name = 'form.html'
+    context = {"title": f"Update {obj.title}", "form": form}
     return render(request, template_name, context)
 
-
+@staff_member_required
 def blog_post_delete_view(request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
     template_name = 'blog/delete.html'
+    if request.method == "POST":
+        obj.delete()
+        return redirect("/blog")
     context = {'object': obj}
     return render(request, template_name, context)
