0ecced28aa1a61c63b70761ee929dbbfc86fd031
ggrobelny <ggrobelny@outlook.fr>
2022-02-03 22:13:59 +0100

    get_absolute_url funkcja w models i linki mam na niebiesko i se klikam i pokazujo mi posty

--- src/blog/models.py
+++ src/blog/models.py
@@ -10,3 +10,7 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True)
     content = models.TextField(null=True, blank=True)
+
+    def get_absolute_url(self):
+        return f"/blog/{self.slug}"
+    
