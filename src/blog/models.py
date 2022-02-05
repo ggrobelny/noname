0b5db063b7d4fae7baa1c2a16955785a9498e5c3
ggrobelny <ggrobelny@outlook.fr>
2022-02-05 17:56:47 +0100

    publish_date, timestamp, updated

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,4 +1,5 @@
 from operator import mod
+from turtle import update
 from django.conf import settings
 from django.db import models
 
@@ -10,15 +11,21 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True)
     content = models.TextField(null=True, blank=True)
+    publish_date = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-publish_date', '-updated', '-timestamp']
 
     def get_absolute_url(self):
         return f"/blog/{self.slug}"
 
 
     def get_edit_url(self):
-        return f"{self.get_absolute_url}/edit"
+        return f"{self.get_absolute_url()}/edit"
 
 
     def get_delete_url(self):
-        return f"{self.get_absolute_url}/delete"
+        return f"{self.get_absolute_url()}/delete"
     
