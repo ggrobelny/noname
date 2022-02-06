c56868fc428c5b9dd7256150ce70d8c7e3efac77
ggrobelny <ggrobelny@outlook.fr>
2022-02-06 09:37:18 +0100

    static media images blank

--- src/blog/models.py
+++ src/blog/models.py
@@ -21,6 +21,7 @@ class BlogPostManager(models.Manager):
 
 class BlogPost(models.Model):
     user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
+    image = models.FileField(upload_to='image/', blank=True, null=True)
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True)
     content = models.TextField(null=True, blank=True)
--- src/noname/settings.py
+++ src/noname/settings.py
@@ -122,6 +122,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+LOCAL_STATIC_CDN_PATH = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn_test')
+STATIC_ROOT = os.path.join(LOCAL_STATIC_CDN_PATH, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles')
+]
+MEDIA_ROOT = os.path.join(LOCAL_STATIC_CDN_PATH, 'media')
+MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
